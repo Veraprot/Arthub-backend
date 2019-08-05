8a5358b350f119dea3b37d0978defeba8a504879
Veraprot <vera.protopopova@gmail.com>
2019-08-05 11:55:38 -0400

    add more routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
 import About from './components/layout/About'
 import Dashboard from './components/Profile/Dashboard'
+import Login from './components/auth/Login'
 import './assets/styles/app.scss'
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <div className="main">
         <Route exact path="/about" component={About} />
         <Route exact path="/user" component={Dashboard} />
+        <Route exact path="/" component={Login}/>
       </div>
     </BrowserRouter>
   );
