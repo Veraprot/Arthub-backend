09c4b1b9d8de6531bee5b448854329e7f5ef574b
Veraprot <vera.protopopova@gmail.com>
2019-08-06 19:32:34 -0400

    check if user is authenticated

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,8 @@
-// import isEmpty from '../validation/is-empty';
+import isEmpty from '../validation/is-empty';
 import { SET_CURRENT_USER } from '../actions/types';
 
 const initialState = {
-  // isAuthenticated: false,
+  isAuthenticated: false,
   user: {}
 };
 
@@ -11,6 +11,7 @@ export default function(state = initialState, action) {
     case SET_CURRENT_USER:
       return {
         ...state,
+        isAuthenticated: !isEmpty(action.payload),
         user: action.payload
       };
     default:
