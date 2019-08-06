ca5bc923e5f9ddcf3643aba78ff127068f577b38
Veraprot <vera.protopopova@gmail.com>
2019-08-06 17:45:29 -0400

    add auth on backend

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,6 +1,8 @@
 // Load User model
 const User = require('../models/User');
-const passwordConfirmation = require('../utils/validation/passwordHelper')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secretOrKey = process.env.SECRET_OR_KEY;
 
 exports.testRoute = (req, res) => {
     res.json({mes: 'what do you want'})
@@ -8,15 +10,38 @@ exports.testRoute = (req, res) => {
 
 exports.registerUser = (req, res) => {
   const {name, email, password, avatar} = req.body
+  
+  // let user = new User({name, email, password, avatar})
+  // user.save() 
+  //   .then(result => {
+  //     res.json({result})
+  //   })
+  //   .catch(err => {
+  //     res.json(err)
+  //   })
+  User.findOne({ email }).then(user => {
+    if (user) {
+      return res.status(400).json({error: 'Email already exists'});
+    } else {
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        avatar,
+        password: req.body.password
+      });
 
-  let user = new User({name, email, password, avatar})
-  user.save() 
-    .then(result => {
-      res.json({result})
-    })
-    .catch(err => {
-      res.json(err)
-    })
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
 }
 
 exports.loginUser = (req, res) => {
@@ -25,11 +50,35 @@ exports.loginUser = (req, res) => {
   User.findOne({email}).then(user => {
     // Check for user
     if (!user) {
-      errors.email = 'User not found';
-      return res.status(404).json(errors);
+      return res.status(404).json({error: 'User not found'});
     }
 
     console.log(user)
+    // check password 
+
+    bcrypt.compare(password, user.password)
+      .then(isMatch => {
+        if (isMatch) {
+          // User Matched
+          const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+  
+          // Sign Token
+          jwt.sign(
+            payload,
+            secretOrKey,
+            { expiresIn: 3600 },
+            (err, token) => {
+              res.json({
+                success: true,
+                token: 'Bearer ' + token
+              });
+            }
+          );
+        } else {
+          errors.password = 'Password incorrect';
+          return res.status(400).json(errors);
+        }
+      })
   })
 }
 
