d07bec818ae769375b12af49f95bd164a8a369bc
Veraprot <vera.protopopova@gmail.com>
2019-08-06 20:29:51 -0400

    logout user after certain time frame

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,27 @@ import Register from './components/auth/Register'
 import { Provider } from 'react-redux';
 import store from './store';
 import './assets/styles/app.scss'
+import setHeaders from './utils/setHeaders';
+import jwt_decode from 'jwt-decode';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  setHeaders(localStorage.jwtToken);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  // Check for expired token
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+    // Redirect to login
+    window.location.href = '/login';
+  }
+}
 
 function App() {
   return (
--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -24,3 +24,9 @@ export const setCurrentUser = decoded => {
     payload: decoded
   };
 };
+
+export const logoutUser = () => dispatch => {
+  localStorage.removeItem('jwtToken');
+  setHeaders(false);
+  dispatch(setCurrentUser({}));
+}
