21ee82253032a122eb812a163ffb2c9bf3e90170
Veraprot <vera.protopopova@gmail.com>
2019-08-26 12:22:16 -0400

    exports name

--- client/src/components/friends/FriendsList.js
+++ client/src/components/friends/FriendsList.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Dashboard() {
+function FriendsList() {
   return (
     <div>
       all the friedns
@@ -8,4 +8,4 @@ function Dashboard() {
   )
 }
 
-export default Dashboard;
+export default FriendsList;
