8fcf3a5e34d1f6bd01b0be598d559068b247fb76
Veraprot <vera.protopopova@gmail.com>
2019-08-29 10:43:11 -0400

    save images on backend

--- server/server.js
+++ server/server.js
@@ -11,7 +11,31 @@ const express = require('express'),
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(multer().single('image'))
+
+const fileStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads')
+  },
+  filename: function (req, file, cb) {
+    console.log(file)
+    cb(null, Date.now() + "-" + file.originalname)
+  }
+})
+
+const fileFilter = (req, file, cb) => {
+  if ( 
+    file.mimetype == "image/png" || 
+    file.mimetype == "image/jpg" || 
+    file.mimetype == "image/jpeg" 
+  ) {
+    cb(null, true)
+  } else {
+    cb(null, false)
+
+  }
+}
+
+app.use(multer({storage: fileStorage, fileFilter}).single('image'))
 
 const corsOptions = {
   origin: '*',
