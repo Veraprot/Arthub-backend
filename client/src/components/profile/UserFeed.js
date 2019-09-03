44bd7a2c597ba186e617934dc03462da6937d064
Veraprot <vera.protopopova@gmail.com>
2019-09-03 17:09:21 -0400

    add layout styles for images

--- client/src/components/profile/UserFeed.js
+++ client/src/components/profile/UserFeed.js
@@ -1,24 +1,25 @@
 import React, {useEffect} from "react";
 import {connect} from 'react-redux'
 import {getItems} from '../../actions/itemActions'
+const resourceRoot = process.env.REACT_APP_RESOURCE_ROOT
 
 function UserFeed(props) {
   useEffect(() => {
     props.getItems(props.currentUser._id)
   }, [])
-  
-  const renderItems = () => {
-    return props.items.map(item => {
-      return (
-        <div key={item._id}>{item.description}</div>
-      )
-    })
-  }
 
   return (
     <div className="profile-feed-container">
-      user feed
-      {renderItems()}
+      {
+        props.items.map(item => {
+          return (
+            <div key={item._id} className="item-container">
+              {item.description}
+              <img src={`${resourceRoot}/${item.image}`} alt=""/>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
