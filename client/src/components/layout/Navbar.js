c3e3b1fea3b99324236e10b387b3bf2c02e8dee6
Veraprot <vera.protopopova@gmail.com>
2019-09-10 16:57:42 -0400

    more styles

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { logoutUser } from '../../actions/userActions'
 import { withRouter } from 'react-router'
 import { Icon } from 'semantic-ui-react';
+const resourceRoot = process.env.REACT_APP_RESOURCE_ROOT
 
 function Navbar(props) {
   const handleClick = () => {
@@ -38,7 +39,10 @@ function Navbar(props) {
         <Icon name='group' size='large' />
       </Link>
       <Link to="/user">
-        <Icon name='user' size='large' />
+        {/* <Icon name='user' size='large' /> */}
+        <div className="avatar-container">
+            <img className="profile-icon" src={`${resourceRoot}/${props.user.info.avatar}`} alt=""/>
+        </div>
       </Link>
     </>
   );
