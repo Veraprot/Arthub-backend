9afb0a47dc260d8fe655f27dee3e5cc069b684b4
Veraprot <vera.protopopova@gmail.com>
2019-12-20 14:28:24 -0500

    remove unused dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar'
 import About from './components/layout/About'
